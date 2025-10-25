0373a46198a5969855b146f2571abae0e8d6ec19
Andrey Larionov <nowiwr01p@icloud.com>
2025-10-25 15:37:22 +0400

    init project - base app modules

---
+++ app_shared/data/build.gradle.kts
@@ -0,0 +1,29 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidKotlinMultiplatformLibrary)
+}
+
+kotlin {
+    androidLibrary {
+        namespace = "tonix.app.app_shared.data"
+        compileSdk = 36
+        minSdk = 24
+    }
+
+    iosArm64()
+    iosSimulatorArm64()
+    jvm()
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser()
+        binaries.library()
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+        }
+    }
+}
