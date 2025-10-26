56ae1e3f5006f88476a1c0b451f56a1aae794b5b
Andrey Larionov <nowiwr01p@icloud.com>
2025-10-27 01:40:23 +0400

    CreateImportWallet - change AppName to AppLogo

--- app_ui_small/src/commonMain/kotlin/tonix/app/app_ui_small/navigation/screen/ui/create_import_wallet/CreateImportWalletMainScreen.kt
+++ app_ui_small/src/commonMain/kotlin/tonix/app/app_ui_small/navigation/screen/ui/create_import_wallet/CreateImportWalletMainScreen.kt
@@ -8,25 +8,25 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.stringResource
 import tonix.app.app_shared.core_ui.components.button.AppButton
 import tonix.app.app_shared.core_ui.components.button.AppButtonState
+import tonix.app.app_shared.core_ui.components.image.AppImage
 import tonix.app.app_shared.core_ui.components.snowflakes_container.SnowflakesContainer
 import tonix.app.app_shared.core_ui.theme.CustomTheme.colors
-import tonix.app.app_shared.core_ui.theme.CustomTheme.shapes
 import tonix.app.app_shared.core_ui.theme.CustomTheme.typography
 import tonix.app.resources.Res
-import tonix.app.resources.app_name
 import tonix.app.resources.create_import_wallet
 import tonix.app.resources.create_new_wallet
+import tonix.app.resources.ic_app_logo_foreground
 import tonix.app.resources.import_existing_wallet
 
 @Composable
@@ -47,7 +47,7 @@ private fun Content(listener: CreateImportWalletListener?) = SnowflakesContainer
             .statusBarsPadding()
             .navigationBarsPadding()
     ) {
-        AppName()
+        AppLogo()
         Spacer(modifier = Modifier.weight(1f))
         Description()
         Spacer(modifier = Modifier.weight(1f))
@@ -57,17 +57,12 @@ private fun Content(listener: CreateImportWalletListener?) = SnowflakesContainer
 }
 
 @Composable
-private fun AppName() {
-    Text(
-        text = stringResource(Res.string.app_name),
-        color = colors.text,
-        style = typography.h1,
+private fun AppLogo() {
+    AppImage(
+        image = Res.drawable.ic_app_logo_foreground,
         modifier = Modifier
-            .clip(shapes.extraLarge)
-            .background(
-                color = colors.background,
-                shape = shapes.extraLarge
-            )
+            .padding(top = 16.dp)
+            .size(64.dp)
     )
 }
 
@@ -81,11 +76,6 @@ private fun Description() {
         modifier = Modifier
             .padding(horizontal = 24.dp)
             .fillMaxWidth()
-            .clip(shapes.extraLarge)
-            .background(
-                color = colors.background,
-                shape = shapes.extraLarge
-            )
     )
 }
 
