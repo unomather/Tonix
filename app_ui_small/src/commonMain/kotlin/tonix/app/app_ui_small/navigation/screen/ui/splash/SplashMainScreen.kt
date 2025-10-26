de31b781aef72152ff587b37440bf4a61abad4a7
Andrey Larionov <nowiwr01p@icloud.com>
2025-10-26 14:59:23 +0400

    AppImage

--- app_ui_small/src/commonMain/kotlin/tonix/app/app_ui_small/navigation/screen/ui/splash/SplashMainScreen.kt
+++ app_ui_small/src/commonMain/kotlin/tonix/app/app_ui_small/navigation/screen/ui/splash/SplashMainScreen.kt
@@ -1,14 +1,12 @@
 package tonix.app.app_ui_small.navigation.screen.ui.splash
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ColorFilter
-import org.jetbrains.compose.resources.painterResource
+import tonix.app.app_shared.core_ui.components.image.AppImage
 import tonix.app.app_shared.core_ui.theme.CustomTheme.colors
 import tonix.app.resources.Res
 import tonix.app.resources.app_logo_foreground
@@ -21,10 +19,9 @@ internal fun SplashMainScreen() {
             .fillMaxSize()
             .background(colors.background)
     ) {
-        Image(
-            painter = painterResource(Res.drawable.app_logo_foreground),
-            colorFilter = ColorFilter.tint(colors.text),
-            contentDescription = ""
+        AppImage(
+            image = Res.drawable.app_logo_foreground,
+            color = colors.text
         )
     }
 }
